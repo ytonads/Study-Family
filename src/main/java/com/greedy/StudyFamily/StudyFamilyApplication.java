cb2a1f28409e5aae78e4a86cf12578edd3495f74
seonghyunki <seong161718@gmail.com>
2022-11-07 16:40:31 +0900

    main

---
+++ src/main/java/com/greedy/StudyFamily/StudyFamilyApplication.java
@@ -0,0 +1,13 @@
+package com.greedy.StudyFamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyFamilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyFamilyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greedy/StudyFamily/StudyFamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greedy.StudyFamily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyFamilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
