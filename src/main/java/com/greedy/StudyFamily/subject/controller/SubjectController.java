8e8df6bd89c741f3d4725dc7741b76a3bb097492
yuseungchul <yoosc14@gmai.com>
2022-11-17 17:44:54 +0900

    subject 추가

--- src/main/java/com/greedy/StudyFamily/subject/controller/SubjectController.java
+++ src/main/java/com/greedy/StudyFamily/subject/controller/SubjectController.java
@@ -1,5 +1,13 @@
 package com.greedy.StudyFamily.subject.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("subject")
 public class SubjectController {
 
+	
 }
--- src/main/java/com/greedy/StudyFamily/subject/service/SubjectService.java
+++ src/main/java/com/greedy/StudyFamily/subject/service/SubjectService.java
@@ -1,5 +1,8 @@
 package com.greedy.StudyFamily.subject.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SubjectService {
 
 }
