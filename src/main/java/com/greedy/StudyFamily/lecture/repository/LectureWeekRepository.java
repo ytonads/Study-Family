4af93d46887d459eebd293284725e02528edf182
경주 신 <tlsrudwn1103@gmail.com>
2022-11-20 01:24:01 +0900

    lecture 오류 잡음

---
+++ src/main/java/com/greedy/StudyFamily/lecture/repository/LectureWeekRepository.java
@@ -0,0 +1,9 @@
+package com.greedy.StudyFamily.lecture.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.greedy.StudyFamily.lecture.entity.LectureWeek;
+
+public interface LectureWeekRepository extends JpaRepository<LectureWeek, Long> {
+
+}
