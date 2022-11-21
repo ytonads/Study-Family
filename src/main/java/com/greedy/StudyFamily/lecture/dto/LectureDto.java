5889460fb692bd6be28a90808692aa3fb6e3a75b
KJ.Shin <tlsrudwn1103@gmail.com>
2022-11-21 19:08:54 +0900

    교수, 학생 강의실 상세 조회 구현

--- src/main/java/com/greedy/StudyFamily/lecture/dto/LectureDto.java
+++ src/main/java/com/greedy/StudyFamily/lecture/dto/LectureDto.java
@@ -1,7 +1,11 @@
 package com.greedy.StudyFamily.lecture.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.multipart.MultipartFile;
 
+import com.greedy.StudyFamily.lecture.entity.LectureWeek;
 import com.greedy.StudyFamily.professor.dto.ProfessorDto;
 import com.greedy.StudyFamily.subject.dto.SubjectDto;
 
@@ -17,6 +21,7 @@ public class LectureDto {
 	private String lectureName;
 	private Long lecturePersonnel;
 	private String openingDate;
+	private List<LectureWeekDto> LectureWeeks;
 	
 	//파일 업로드 로직 작성 시 활용할 필드
 //	private MultipartFile lectureFile;
--- src/main/java/com/greedy/StudyFamily/lecture/repository/LectureWeekRepository.java
+++ src/main/java/com/greedy/StudyFamily/lecture/repository/LectureWeekRepository.java
@@ -1,9 +1,18 @@
 package com.greedy.StudyFamily.lecture.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.greedy.StudyFamily.lecture.entity.Lecture;
 import com.greedy.StudyFamily.lecture.entity.LectureWeek;
+import com.greedy.StudyFamily.student.entity.Student;
 
 public interface LectureWeekRepository extends JpaRepository<LectureWeek, Long> {
+
+	Optional<LectureWeek> findByLectures_LectureCode(Long lectureCode);
+
+
+
 
 }
