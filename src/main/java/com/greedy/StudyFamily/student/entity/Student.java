0d7ce879dcb5b38ccee750f0e1f63be670589d04
kim <dpttm2@gmail.com>
2022-11-30 16:19:08 +0900

    11.30 수강취소 코드 수정

--- src/main/java/com/greedy/StudyFamily/student/entity/Student.java
+++ src/main/java/com/greedy/StudyFamily/student/entity/Student.java
@@ -73,7 +73,9 @@ public class Student {
 	private String nationality;
 	
 	/* 추가 */
-	
+	@ManyToOne
+	@JoinColumn(name = "SCHOOL_STATUS_CODE")
+	private SchoolStatus schoolStatus;
 
 
 	@OneToMany(mappedBy = "student")
@@ -88,7 +90,7 @@ public class Student {
 
 	/* 학생정보 수정 용도 메소드 정의 */
 	public void update(String studentCode, String studentName, String admissionsDay, Department department, String studentRegistNum,
-			String grade, String gender, String studentEmail, String studentPhone, String studentAddress, String nationality) {
+			String grade, String gender, String studentEmail, String studentPhone, String studentAddress, String nationality, SchoolStatus schoolStatus) {
 
 		this.studentCode = studentCode;
 		this.studentName = studentName;
@@ -101,6 +103,7 @@ public class Student {
 		this.studentPhone = studentPhone;
 		this.studentAddress = studentAddress;
 		this.nationality = nationality;
+		this.schoolStatus = schoolStatus;
 	
 }
 
